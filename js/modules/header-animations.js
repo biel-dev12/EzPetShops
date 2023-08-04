5ee4fb962b476dd10a7ee3ee17c9e3a89bad1148
Gabriel Carvalho <gabriel.carvalho.pg@gmail.com>
2023-08-04 11:58:26 -0300

    finalizacao do header para mobile

--- js/modules/header-animations.js
+++ js/modules/header-animations.js
@@ -1,33 +1,49 @@
 // menu-hamb animation
 export function hambAnim() {
-  const elMenu = document.querySelector(".menu-hamb");
+  const menuHamb = document.querySelector(".menu-hamb");
   const hambElements = [
-    document.querySelector("span.hamb-top"),
-    document.querySelector("span.hamb-middle"),
-    document.querySelector("span.hamb-bottom"),
+    document.querySelector(".line1"),
+    document.querySelector(".line2"),
+    document.querySelector(".line3"),
   ];
-  elMenu.addEventListener("click", () => {
+  const navList = document.querySelector("#nav-list");
+  menuHamb.addEventListener("click", () => {
     hambElements.forEach((el) => {
       el.classList.toggle("anim");
     });
+
+    navList.classList.toggle("show-nav");
   });
 }
 
 //Dropdown animation
 export function dropdownAnim() {
   const elDropdown = document.querySelector(".dropdown");
-  const elOptionDrop = document.querySelector(".drop");
+  const elOptionDrop = document.querySelector(".li-drop>span");
+  const arrowIcon = document.querySelector(".arrow");
+
   let isVisible = false;
+
   elOptionDrop.addEventListener("click", () => {
     elDropdown.classList.toggle("show");
+    arrowIcon.classList.remove("bi-caret-right");
+    arrowIcon.classList.add("bi-caret-down-fill");
     isVisible = !isVisible;
+    if(isVisible === false){ 
+      arrowIcon.classList.remove("bi-caret-down-fill");
+      arrowIcon.classList.add("bi-caret-right");
+    }
   });
   //verficando se o click foi e qualquer lugar sem ser o dropdown
   document.addEventListener("click", (ev) => {
     const targetEl = ev.target;
     if(targetEl !== elOptionDrop && !elDropdown.contains(targetEl)){
-      elDropdown.classList.remove("show")
+      elDropdown.classList.remove("show");
+      // arrowIcon.classList.remove("bi-caret-down-fill");
+      arrowIcon.classList.add("bi-caret-right");
       isVisible = false;
     }
     });
 }
+
+
