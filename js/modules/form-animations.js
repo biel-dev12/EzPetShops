c542e032a0f921f8f23408f419f421e0991edd36
Gabriel Carvalho <gabriel.carvalho.pg@gmail.com>
2023-08-11 11:03:01 -0300

    Continuacao da estilizacao do form para mobile

--- js/modules/form-animations.js
+++ js/modules/form-animations.js
@@ -3,6 +3,15 @@ export function toggleForm(){
     const toggleSignup = document.querySelector(".link-signup"); 
     const boxLogin = document.querySelector(".login");
     const boxSignup = document.querySelector(".register");
+    const btnLogin = document.querySelector(".btn-login");
+    const btnSignup = document.querySelector(".btn-signup");
+    const navList = document.querySelector("#nav-list");
+    const cardContainer = document.querySelector(".card-container");
+    const hambElements = [
+        document.querySelector(".line1"),
+        document.querySelector(".line2"),
+        document.querySelector(".line3"),
+      ];
 
     toggleLogin.addEventListener("click", ()=>{
         boxSignup.classList.toggle("remove-register");
@@ -11,5 +20,23 @@ export function toggleForm(){
     toggleSignup.addEventListener("click", ()=>{
         boxSignup.classList.toggle("remove-register");
         boxLogin.classList.toggle("show-login");
+    });
+    btnSignup.addEventListener("click", ()=>{
+        boxSignup.classList.remove("remove-register");
+        boxLogin.classList.remove("show-login");
+        navList.classList.remove("show-nav");
+        cardContainer.classList.add("show-card");
+        hambElements.forEach((el) => {
+            el.classList.remove("anim");
+          });
+    });
+    btnLogin.addEventListener("click", ()=>{
+        boxLogin.classList.add("show-login");
+        boxSignup.classList.add("remove-register");
+        navList.classList.remove("show-nav");
+        cardContainer.classList.add("show-card");
+        hambElements.forEach((el) => {
+            el.classList.remove("anim");
+          });
     });
 }
