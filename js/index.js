829a47a0bb1b1f8ccac62ebab130313bd6845bde
Gabriel Carvalho <gabriel.carvalho.pg@gmail.com>
2023-08-16 10:14:02 -0300

    Manutencao do layout das paginas

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 import { hambAnim, dropdownAnim } from "./modules/header-animations.js";
 import { toggleForm } from "./modules/form-animations.js";
-import { activeLabel } from "./modules/info-pages.js";
 
 hambAnim();
 dropdownAnim();
 toggleForm();
-activeLabel();
